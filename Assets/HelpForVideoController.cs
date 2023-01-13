4706c5037544887dfda0ad550d39037d3c21fac4
Dlaranavas1996 <dlaranavas1996@gmail.com>
2023-01-13 14:46:18 +0100

    Añadida función para video mapa
    Añadida función para video mapa

---
+++ Assets/HelpForVideoController.cs
@@ -0,0 +1,25 @@
+using Map;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpForVideoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public MapManager mp;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+
+            mp.GenerateNewMap();
+        }
+    }
+}
